56a2df4bd47ba4b8dafa9b5403fdfbb0c937b7c1
Everton Figueiredo <j.everton.figueiredo@gmail.com>
2023-01-30 14:10:22 -0300

    Atualização

--- src/app.js
+++ src/app.js
@@ -15,11 +15,22 @@ db.connect();
 import usarioRoutes from "./routes/usuarioRouter.js"
 
 app.use("/usuario", usarioRoutes)
-app.use("/", (req,res) => {
+
+app.use("/", (req, res) => {
     res.status(200).json({
         statusCode: 200,
         message: "Teste"
     })
 })
+
+
+app.get('/api', (req, res) => {
+    const randomId = `${Math.random()}`.slice(2);
+    const path = `/api/item/${randomId}`;
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+    res.end(`Hello! Fetch one item: <a href="${path}">${path}</a>`);
+});
+
 
 export default app;
